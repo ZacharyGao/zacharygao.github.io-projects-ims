b4b7ad4b4d83f73165a91b28ce04969a29f0a708
Zach Gao <zihegao@outlook.com>
2023-12-18 20:50:29 +0000

    test

--- js/script.js
+++ js/script.js
@@ -56,12 +56,12 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    
+
     var currentPage = window.location.pathname.split("/").pop();
     var navItems = document.querySelectorAll(".sidenav ul li[data-page]");
 
-    navItems.forEach(function(item) {
-        if(item.getAttribute('data-page') === currentPage.replace('.php', '')) {
+    navItems.forEach(function (item) {
+        if (item.getAttribute('data-page') === currentPage.replace('.php', '')) {
             item.classList.add('active');
         }
         else {
@@ -88,7 +88,7 @@ function ajaxFormSubmit(formSelector, targetUrl) {
                 var tableID = "searchPeople";
                 var columnNames = ["People_name", "People_license"];
                 if (inputElement && document.getElementById(tableID)) {
-                    inputElement.onkeyup = function() {
+                    inputElement.onkeyup = function () {
                         filterTableByName("info", tableID, columnNames);
                     };
                 }
@@ -561,7 +561,7 @@ function addNewPersonToDatabase() {
     xhttp.send("personName=" + personName + "&licenceNum=" + licenceNum + "&personDOB=" + personDOB + "&penaltyPoints=" + penaltyPoints + "&address=" + address);
 
 
-    
+
     // var formData = new FormData(this);
     // var xhr = new XMLHttpRequest();
     // xhr.open("POST", targetUrl, true);
@@ -577,7 +577,7 @@ function addNewPersonToDatabase() {
 
 
 if (document.getElementById("query_people")) {
-    document.getElementById("query_people").addEventListener("keydown", function(event) {
+    document.getElementById("query_people").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
             document.querySelector("#query_people button[type='submit']").click();
@@ -590,3 +590,32 @@ if (document.getElementById("query_people")) {
 //         document.querySelector("#query_people button[type='submit']").click();
 //     }
 // });
+
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain attribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+};
