35843097fbb1d66ea59671f27d913eb399d0a720
Zach Gao <zihegao@outlook.com>
2023-12-18 21:00:52 +0000

    test

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var navItems = document.querySelectorAll(".sidenav ul li[data-page]");
 
     navItems.forEach(function (item) {
-        if (item.getAttribute('data-page') === currentPage.replace('.php', '')) {
+        if (item.getAttribute('data-page') === currentPage.replace('.html', '')) {
             item.classList.add('active');
         }
         else {
@@ -594,27 +594,27 @@ if (document.getElementById("query_people")) {
 
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
+    // loop through a collection of all HTML elements
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
-        /*search for elements with a certain attribute:*/
+        // search for elements with a certain attribute
         file = elmnt.getAttribute("w3-include-html");
         if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
+            // make an HTTP request using the attribute value as the file name:
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
                     if (this.status == 200) { elmnt.innerHTML = this.responseText; }
                     if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    /*remove the attribute, and call this function once more:*/
+                    // remove the attribute, and call this function once more
                     elmnt.removeAttribute("w3-include-html");
                     includeHTML();
                 }
             }
             xhttp.open("GET", file, true);
             xhttp.send();
-            /*exit the function:*/
+            // exit the function
             return;
         }
     }
