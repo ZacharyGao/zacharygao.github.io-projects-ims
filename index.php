abfd392082622ac4a016f52ca75c6bb210c3b700
Zach Gao <zihegao@outlook.com>
2023-12-18 03:18:07 +0000

    test

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
+```
+
+## 2.2. PHP Syntax
+
+- A PHP script can be placed anywhere in the document.
+- A PHP script starts with `<?php` and ends with `?>`:
+
+```php
+<?php
+    // PHP code goes here
+?>
+```
+
+- A PHP file normally contains HTML tags, and some PHP scripting code.
+- Below, we have an example of a simple PHP file, with a PHP script that uses a built-in PHP function "echo" to output the text "Hello World!" on a web page:
+
+```php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
+```
